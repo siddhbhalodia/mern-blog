875863cf57a922867be966cc8f64e1bb10003498
vatsal thummar <tvatsal04@gmail.com>
2024-02-02 16:41:33 +0530

    Database and Server connected

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Mongodb connected')
+}).catch((err)=>{
+    console.log(err)
+})
+const app = express();
+
+app.listen(3000,()=>{
+    console.log("server is running on 3000")
+})
