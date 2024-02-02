1c53035d18de88e0077c143a85d158fa193a950b
siddhbhalodia <bhalodiasid991010@gmail.com>
2024-02-02 16:20:17 +0530

    pages and header created

---
+++ blog/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/sign-in" element={<Signin />}/>
+      <Route path="/dashboard" element={<Dashboard />}/>
+      <Route path="/sign-up" element={<Signup />}/>
+      <Route path="/projects" element={<Projects />}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
