c91e273d437ae406369c13353007b610b39d28aa
siddhbhalodia <bhalodiasid991010@gmail.com>
2024-02-04 15:18:39 +0530

    Sign Up page UI created

--- blog/src/pages/Signup.jsx
+++ blog/src/pages/Signup.jsx
@@ -1,7 +1,59 @@
+import { Button, Label, TextInput } from 'flowbite-react'
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div className='min-h-screen mt-20'>
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1">
+        <Link to="/" className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Nebula</span>
+            Blog
+        </Link>
+        <p className='text-sm mt-5'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero illo quos dolorum, dolores provident saepe minima dignissimos accusantium perspiciatis ex recusandae nulla corrupti corporis rerum repellat, aspernatur sit. Minus, ab.
+        </p>
+        </div>
+        {/* right */}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4'>
+            <div className="">
+              <Label value='username'/>
+              <TextInput
+              type='text'
+              placeholder='Username'
+              id='username'
+              />
+            </div>
+            <div className="">
+              <Label value='Email'/>
+              <TextInput
+              type='text'
+              placeholder='name@company.com'
+              id='email'
+              />
+            </div>
+            <div className="">
+              <Label value='Password'/>
+              <TextInput
+              type='text'
+              placeholder='Password'
+              id='password'
+              />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit'>
+              Sign-up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to='/sign-in' className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
