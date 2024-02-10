21d9b536478d51adfe45b592180f6cf7fc9621b7
vatsal thummar <tvatsal04@gmail.com>
2024-02-10 18:30:28 +0530

    Dark Theme Added

--- blog/src/redux/store.js
+++ blog/src/redux/store.js
@@ -3,9 +3,10 @@ import userReducer from '/src/redux/user/userSlice.js';
 import {persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 import persistStore from 'redux-persist/es/persistStore';
-
+import themeReducer from './theme/themeSlice';
 const rootReducer = combineReducers({
   user: userReducer,
+  theme:themeReducer,
 });
 
 const persistConfig = {
