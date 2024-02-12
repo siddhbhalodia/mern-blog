4fe22c484b3b6882dea7bef2e8adc081524b124e
siddhbhalodia <bhalodiasid991010@gmail.com>
2024-02-12 23:40:33 +0530

    added update user api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -46,7 +46,7 @@ export const signin = async(req,res,next) => {
         
         const{password: pass, ...rest} = validUser._doc;
         
-        res.status(200).cookie('acces_token',token,{
+        res.status(200).cookie('access_token',token,{
             httpOnly : true
         }).json(rest);
     } catch(error){
