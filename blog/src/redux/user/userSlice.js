ccc73afb90e3d3a90091ed70adaa94e2eebcb21c
Galani Aditya <galaniaaditya@gmail.com>
2024-02-20 23:33:41 +0530

    completed delete user account functionality

--- blog/src/redux/user/userSlice.js
+++ blog/src/redux/user/userSlice.js
@@ -36,9 +36,22 @@ export const userSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         },
+        deleteUserStart: (state) => {
+            state.loading=true;
+            state.error=null;
+        },
+        deleteUserSuccess: (state) =>{
+            state.currentUser=null;
+            state.loading=false;
+            state.error=null;
+        },
+        deleteUserFailure:(state,action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
     },
 });
 
-export const { signInFailure,signInStart,signInSuccess,updateFailure,updateStart,updateSuccess } = userSlice.actions;
+export const { signInFailure,signInStart,signInSuccess,updateFailure,updateStart,updateSuccess,deleteUserFailure,deleteUserSuccess,deleteUserStart } = userSlice.actions;
 
 export default userSlice.reducer;
