53487e92b08566b907e818188bfff92db5043bad
siddhbhalodia <bhalodiasid991010@gmail.com>
2024-02-22 19:10:40 +0530

    added create a post api route

--- blog/src/pages/CreatePost.jsx
+++ blog/src/pages/CreatePost.jsx
@@ -8,8 +8,7 @@ export default function CreatePost() {
     <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
     <form className='flex flex-col gap-4'>
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
-            <TextInput type='text' placeholder='Title' required id='title'
-            className='flex-1'/>
+            <TextInput type='text' placeholder='Title' required id='title' className='flex-1'/>
             <Select>
                 <option value='uncategorized'>Select a category</option>
                 <option value='javascript'>JavaScript</option>
