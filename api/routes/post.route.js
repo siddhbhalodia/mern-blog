cd06695e31bb5a1020f13b1b9c818d4166d34d73
vatsal thummar <tvatsal04@gmail.com>
2024-03-26 17:10:57 +0530

    Created getposts api

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { create } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
-
+import { getposts } from "../controllers/post.controller.js";
 const router=express.Router();
 
 router.post('/create',verifyToken,create)
-
+router.get('/getposts',getposts)
 export default router;
