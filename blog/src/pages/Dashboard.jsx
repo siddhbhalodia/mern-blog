09c92c0ebae5969f2c5ce240f6beb37fe176cff7
vatsal thummar <tvatsal04@gmail.com>
2024-03-26 16:23:22 +0530

    Added posts section to the dashboard

--- blog/src/pages/Dashboard.jsx
+++ blog/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 import DashSidebar from "../components/DashSidebar"
 import DashProfile from "../components/DashProfile"
-
+import DashPosts from "../components/DashPosts"
 import React from "react"
 
 export default function Dashboard() {
@@ -11,19 +11,21 @@ export default function Dashboard() {
   useEffect(()=>{
     const urlParams=new URLSearchParams(location.search)
     const tabFromUrl=urlParams.get('tab')
-    console.log(tabFromUrl)
+    // console.log(tabFromUrl)
     if(tabFromUrl){
       setTab(tabFromUrl);
     }
   },[location.search])
   return <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
-        {/* Sidebar */}
+        {/* Sidebar... */}
         <DashSidebar/>
         
       </div>
-        {/* Profile */}
+        {/* Profile... */}
         {tab==='profile' && <DashProfile/>}
+        {/* posts...  */}
+        {tab==='posts' && <DashPosts/>}
     </div>
   
 }
