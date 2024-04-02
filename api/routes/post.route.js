12d2b3cdcfc38efff9cc4b577564ade887f146fc
Galani Aditya <galaniaaditya@gmail.com>
2024-04-03 00:13:00 +0530

    added update post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { create, deletepost } from "../controllers/post.controller.js";
+import { create, deletepost, updatepost } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { getposts } from "../controllers/post.controller.js";
 const router=express.Router();
@@ -7,4 +7,5 @@ const router=express.Router();
 router.post('/create',verifyToken,create)
 router.get('/getposts',getposts)
 router.delete('/deletepost/:postId/:userId',verifyToken,deletepost)
+router.put('/updatepost/:postId/:userId',verifyToken,updatepost)
 export default router;
--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -11,6 +11,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import CreatePost from './pages/CreatePost'
+import UpdatePost from './pages/UpdatePost'
 
 export default function App() {
   return (
@@ -25,6 +26,7 @@ export default function App() {
       </Route>
       <Route element={<OnlyAdminPrivateRoute/>}>
       <Route path="/create-post" element={<CreatePost />}/>
+      <Route path="/update-post/:postId" element={<UpdatePost />}/>
       </Route>
       <Route path="/sign-up" element={<Signup />}/>
       <Route path="/projects" element={<Projects />}/>
