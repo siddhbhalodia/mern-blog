70dfc2999a83ba842fd8cdb95e4a5669f03a8874
Galani Aditya <galaniaaditya@gmail.com>
2024-04-02 20:41:08 +0530

    added show more functionality to the posts results of the dashboard

--- blog/src/components/DashPosts.jsx
+++ blog/src/components/DashPosts.jsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom'
 function DashPosts() {
   const {currentUser} = useSelector((state)=>state.user)
   const [userPosts,setUserPosts] = useState([])
+  const [showMore,setShowMore] = useState(true);
   console.log(userPosts);
   useEffect(()=>{
       const fetchPosts = async()=>{
@@ -15,7 +16,9 @@ function DashPosts() {
           const data = await res.json()
           if(res.ok){
             setUserPosts(data.posts)
-
+            if(data.posts.length < 9){
+              setShowMore(false)
+            }
           }
         } catch(error){
           console.log(error.message);
@@ -25,6 +28,27 @@ function DashPosts() {
         fetchPosts();
       }
   },[currentUser._id])
+
+  const handleShowMore = async() =>{
+    const startIndex = userPosts.length;
+
+    try{
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`)
+      const data = await res.json()
+      if(res.ok){
+        setUserPosts((prev) => [...prev,...data.posts]);
+        if(data.posts.length < 9){
+          setShowMore(false)
+        }
+      }
+      else{
+        setUserPosts(userPosts.filter((post)=>post._id !== postId))
+      }
+    }catch(error){
+      console.log(error.message)
+    }
+  }
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 
             scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500 '>
@@ -70,6 +94,13 @@ function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+          {
+            showMore && (
+              <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                Show more
+              </button>
+            )
+          }
         </>
       ) : (
 
