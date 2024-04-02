1d17506b55fe60cfd9a79f39153489773bfd677e
Galani Aditya <galaniaaditya@gmail.com>
2024-04-02 23:03:54 +0530

    added delete post functionality to dashboard

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { create } from "../controllers/post.controller.js";
+import { create, deletepost } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { getposts } from "../controllers/post.controller.js";
 const router=express.Router();
 
 router.post('/create',verifyToken,create)
 router.get('/getposts',getposts)
+router.delete('/deletepost/:postId/:userId',verifyToken,deletepost)
 export default router;
--- blog/src/components/DashPosts.jsx
+++ blog/src/components/DashPosts.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useState } from 'react'
-import { Table } from 'flowbite-react'
+import { Table,Button } from 'flowbite-react'
 import { Link } from 'react-router-dom'
+import { Modal } from 'flowbite-react'
+import { HiOutlineExclamationCircle } from 'react-icons/hi'
 
 function DashPosts() {
   const {currentUser} = useSelector((state)=>state.user)
   const [userPosts,setUserPosts] = useState([])
   const [showMore,setShowMore] = useState(true);
+  const [showModal,setShowModal] = useState(false);
+  const [postIdToDelete,setPostIdToDelete] = useState('');
   console.log(userPosts);
   useEffect(()=>{
       const fetchPosts = async()=>{
@@ -49,6 +53,26 @@ function DashPosts() {
     }
   }
 
+  const handleDeletePost = async()=>{
+    setShowModal(false);
+
+    try{
+      const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}` , {
+        method: 'DELETE',
+      })
+      const data = await res.json()
+      if(res.ok){
+        setUserPosts((prev) => 
+          prev.filter((post) => post._id !== postIdToDelete))
+      }
+      else{
+        console.log(data.message)
+      }
+    }catch(error){
+      console.log(error.message)
+    }
+  }
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 
             scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500 '>
@@ -81,7 +105,10 @@ function DashPosts() {
                   <Table.Cell><Link className='font-medium text-gray-900 dark:text-white' to={`/post/${post.slug}`}>{post.title}</Link> </Table.Cell>
                   <Table.Cell>{post.catagory}</Table.Cell>
                   <Table.Cell>
-                    <span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
+                    <span onClick={()=>{
+                      setShowModal(true);
+                      setPostIdToDelete(post._id);
+                    }} className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
                   </Table.Cell>
                   <Table.Cell>
                     <Link className='text-teal-500 hover:underline cursor-pointer' to={`/update-post/${post._id}`}>
@@ -106,6 +133,24 @@ function DashPosts() {
 
         <p>You have no posts yet</p>
       )}
+
+        <Modal show={showModal} onClose={() => setShowModal(false)} popup size='md'>
+          <Modal.Header/>
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto'/>
+              <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>Are you sure you want to delete your account?</h3>
+              <div className='flex justify-center gap-8'>
+                <Button color='failure' onClick={handleDeletePost}>
+                  Yes, I'm sure
+                </Button>
+                <Button color='gray' onClick={() => setShowModal(false)}>
+                  No, cancel
+                </Button>
+              </div>
+            </div>
+          </Modal.Body>
+          </Modal>
     </div>
 
   )
