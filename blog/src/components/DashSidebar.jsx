de62d53c59abb9181695f4d2c7f8e6a3e0c818f8
Galani Aditya <galaniaaditya@gmail.com>
2024-04-05 23:21:36 +0530

    added comments section to the admin dashboard

--- blog/src/components/DashSidebar.jsx
+++ blog/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from "flowbite-react"
-import {HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'
+import {HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'
 import { useEffect, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
@@ -59,16 +59,27 @@ export default function DashSidebar() {
           </Link>
           )}
           {currentUser.isAdmin && (
-          <Link to='/dashboard?tab=users'>
-            <Sidebar.Item 
-            active ={tab==='users'}
-            icon={HiOutlineUserGroup}
-            as='div'
-            >
-              Users
-            </Sidebar.Item>
-            
-          </Link>
+            <>
+            <Link to='/dashboard?tab=users'>
+              <Sidebar.Item 
+              active ={tab==='users'}
+              icon={HiOutlineUserGroup}
+              as='div'
+              >
+                Users
+              </Sidebar.Item>
+              
+            </Link>
+            <Link to='/dashboard?tab=comments'>
+              <Sidebar.Item 
+              active ={tab==='comments'}
+              icon={HiAnnotation}
+              as='div'
+              >
+                Comments
+              </Sidebar.Item>
+            </Link>
+            </>
           )}
           <Sidebar.Item icon={HiArrowSmRight}  className='cursor-pointer' onClick={handleSignout}>
             Sign Out
--- blog/src/pages/Dashboard.jsx
+++ blog/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import DashSidebar from "../components/DashSidebar"
 import DashProfile from "../components/DashProfile"
 import DashPosts from "../components/DashPosts"
 import DashUsers from "../components/DashUsers"
+import DashComments from "../components/DashComments"
 import React from "react"
 
 export default function Dashboard() {
@@ -29,6 +30,7 @@ export default function Dashboard() {
         {tab==='posts' && <DashPosts/>}
         {/* users...  */ }
         {tab==='users' && <DashUsers/>}
+        {/* comments...  */}
+        {tab==='comments' && <DashComments/>}
     </div>
-  
 }
