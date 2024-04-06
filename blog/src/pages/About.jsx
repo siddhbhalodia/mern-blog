d869a61f92d8334a67c54774bb57b80f0b1737f9
siddhbhalodia <bhalodiasid991010@gmail.com>
2024-04-06 14:41:34 +0530

    completed about and projects pages

--- blog/src/pages/About.jsx
+++ blog/src/pages/About.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className='text-3xl font-semibold text-center my-7'>About Nebula Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>Nebula blog is a blog that i created to sharer my thoughts and ideas with the world. I am software engineer and i love to write my experiences and things that i have learnt. I hope you enjoy reading my blog.</p>
+            <p>Nebula blog is a blog that i created to sharer my thoughts and ideas with the world. I am software engineer and i love to write my experiences and things that i have learnt. I hope you enjoy reading my blog.</p>
+            <p>Nebula blog is a blog that i created to sharer my thoughts and ideas with the world. I am software engineer and i love to write my experiences and things that i have learnt. I hope you enjoy reading my blog.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
